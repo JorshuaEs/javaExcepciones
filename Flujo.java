9d8a72bca08e69ae6976913fe62d1ad934bbf3ca
JorshuaEs <estradajorshua16@gmail.com>
2023-02-10 01:05:08 -0600

    creacion de nueva exception

--- Flujo.java
+++ Flujo.java
@@ -9,27 +9,16 @@ public class Flujo {
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		metodo2();
+		try {
+			metodo2();
+		}catch(MiException me) {
+			me.printStackTrace();
+		}
 		System.out.println("Fim do metodo1");
 	}
 	
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		for(int i = 1; i <= 5; i++) {
-			System.out.println(i);
-			
-			try {//intenta esto
-				//int num = 0;
-				//int resultado = i/num;
-				//System.out.println(resultado);
-				String test = null;
-				System.out.println(test.toString());
-				
-			}catch(ArithmeticException | NullPointerException exception) {//Atrapa error
-				System.out.println("Atrapo exception");
-				exception.printStackTrace();
-			}
-			}
-		System.out.println("Fim do metodo2");
+		throw new MiException("Mi excepcion fue lanzada");
 	}
 }
